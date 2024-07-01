68e7128f91cfdd84261172bafd61e61f44bfa1d7
Patrix <patrixon9999@wp.pl>
2024-07-01 18:12:47 +0200

    Fixed compilation error

--- union-api/Union/Thirdparty/RTVersion.h
+++ union-api/Union/Thirdparty/RTVersion.h
@@ -2,9 +2,9 @@
 #ifndef __UNION_RT_VERSION_H__
 #define __UNION_RT_VERSION_H__
 #include <crtversion.h>
-#if _VC_CRT_MAJOR_VERSION >= 14 && _VC_CRT_MINOR_VERSION >= 40
+#if _VC_CRT_MAJOR_VERSION >= 14 && _VC_CRT_MINOR_VERSION >= 50
 #error The newest version of the compiler has been detected. Add this version here and compile all static dependencies.
-#elif _VC_CRT_MAJOR_VERSION >= 14 && _VC_CRT_MINOR_VERSION >= 30
+#elif _VC_CRT_MAJOR_VERSION >= 14 && _VC_CRT_MINOR_VERSION >= 40
 #define _VCRT_V143
 #elif _VC_CRT_MAJOR_VERSION >= 14 && _VC_CRT_MINOR_VERSION >= 20
 #define _VCRT_V142
